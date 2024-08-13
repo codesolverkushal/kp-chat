3fbc0d2d94827e44694950487fcc31a57d299fc1
kushal <mohitsinghkpsc@gmail.com>
2024-08-13 09:13:22 +0530

    onlineUsers removed

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -26,13 +26,7 @@ const AppLayout = () => (WrappedComponent) => {
             }}
             height={"100%"}
           >
-            <ChatList chats={sampleChats} chatId={chatId} 
-              newMessagesAlert={[{
-                chatId,
-                count:4
-              }]}
-              onlineUsers={["1","2"]}
-            />
+            <ChatList chats={sampleChats} chatId={chatId} />
           </Grid>
           <Grid item xs={12} sm={8} md={5} lg={6} height={"100%"}>
             <WrappedComponent {...props} />
